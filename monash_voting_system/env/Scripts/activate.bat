61ba5b614f2e0c0896a34057936f0b271b070a4e
PatrickMao0 <145909024+PatrickMao0@users.noreply.github.com>
2025-03-31 22:19:46 +1100

    Fix logo not displayed
    Fixing logo not being displayed issue, adding documents for cloud deployment.

--- monash_voting_system/env/Scripts/activate.bat
+++ monash_voting_system/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\patri\monash_voting_system\env
+set VIRTUAL_ENV=C:\Users\patri\Desktop\monash_voting\env
 
 if not defined PROMPT set PROMPT=$P$G
 
