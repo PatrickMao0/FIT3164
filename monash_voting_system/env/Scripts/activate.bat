c33c30336ec4da2231095af7af8148c0310fa837
PatrickMao0 <145909024+PatrickMao0@users.noreply.github.com>
2025-05-12 16:31:43 +1000

    Update sample user data
    Adding mock up user and details for visualization purpose.

--- monash_voting_system/env/Scripts/activate.bat
+++ monash_voting_system/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\patri\Desktop\monash_voting\env
+set VIRTUAL_ENV=C:\Users\patri\Desktop\FIT3164\monash_voting_system\env
 
 if not defined PROMPT set PROMPT=$P$G
 
